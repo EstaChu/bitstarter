934fc7da4f9c864f24d2c71f7ee5f864559f67de
Jose F. Gonzalez Krause <jflokg@gmail.com>
2013-07-10 17:53:34 +0200

    MODIFIED to serve raw HTML files and ADDED index.html

--- web.js
+++ web.js
@@ -1,11 +1,31 @@
+#!/usr/bin/env node
+// ##### imports
 var express = require('express');
+var fs = require('fs');
 
+
+// ##### app settings
 var app = express.createServer(express.logger());
+var templatePath = '.' // IMPORTANT set to . if no specific path available
+ 
 
+// ##### helper functions
+var rendHtml = function(file2rend){
+    // *html render:*
+    // Reads the file synchronously and returns a buffered string
+    file = templatePath + '/' + file2rend;
+    buffer = new Buffer(fs.readFileSync(file));
+    return buffer.toString();
+}
+
+
+// ##### app main behavior
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(rendHtml('index.html'));
 });
 
+
+// ##### Server launch instance
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
